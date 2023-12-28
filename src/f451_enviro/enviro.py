5a244734e8cea9193b8aad8e2d72a2f99ed2a224
Martin Lanser <martinlanser@gmail.com>
2023-12-27 19:52:10 -0500

    fix 'display_graph'

--- src/f451_enviro/enviro.py
+++ src/f451_enviro/enviro.py
@@ -628,7 +628,7 @@ class Enviro:
             return tuple(int(x * 255.0) for x in colorsys.hsv_to_rgb(color, 1.0, 1.0))
 
         def _get_rgb_from_map(val, limits, colorMap):
-            # Map value against color map. Not taht we're mapping original 
+            # Map value against color map. Note that we're mapping original 
             # value against the color map as the color map limits use
             # actual full-scale values.
             if val > round(limits[2], 1):
@@ -663,17 +663,17 @@ class Enviro:
 
         # Reserve space for progress bar? Then clear rest of display by 
         # 'painting' it black.
-        yMin = 2 if (self.displProgress) else 0
+        yMin = DISPL_TOP_BAR + (2 if (self.displProgress) else 0)
+        yMax = displHeight - yMin
         if minMax is None or minMax[1] == minMax[0]:
             vMin = min(values) if minMax is None else minMax[0]
             vMax = max(values) if minMax is None else minMax[1]
         else:
             vMin, vMax = minMax
 
-        # fitted = [_scale(v, (vMin, vMax), displHeight - yMin) for v in values]
-        fitted = [int(_clamp(_scale(v, (vMin, vMax), displHeight - yMin), yMin, displHeight)) for v in values]
-
-        self._draw.rectangle((0, yMin, displWidth, displHeight), RGB_BLACK)
+        fitted = [int(_clamp(_scale(v, (vMin, vMax), yMax - yMin), yMin, yMax)) for v in values]
+        # self._draw.rectangle((0, yMin, displWidth, yMax), RGB_BLACK)
+        self._draw.rectangle((0, 0, displWidth, displHeight), RGB_BLACK)
 
         # Get colors based on limits and color map? Or generate based on
         # value itself compared to defined limits?
@@ -688,7 +688,7 @@ class Enviro:
             colors = [_get_rgb(v) for v in scaled]
 
         for i in range(len(fitted)):
-            self._draw.rectangle((i, (displHeight + yMin - fitted[i]), i + 1, displHeight), colors[i]) # type: ignore
+            self._draw.rectangle((i, (displHeight - fitted[i]), i + 1, displHeight - 1), colors[i]) # type: ignore
             # --- DEBUG ---
             print(f"F={fitted[i]:.1f} : V={values[i]:.1f} : C={colors[i]}")
             # -------------

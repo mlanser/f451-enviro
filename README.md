f9c229dd7d5c3d6bbea788e914c8a6ef9c89b4bd
Martin Lanser <martinlanser@gmail.com>
2023-12-31 17:24:56 -0500

    bump version 1.2.0 -> 1.2.1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# f451 Labs Enviro+ module v1.2.0
+# f451 Labs Enviro+ module v1.2.1
 
 ## Overview
 
--- src/f451_enviro/__init__.py
+++ src/f451_enviro/__init__.py
@@ -1,4 +1,4 @@
 """f451 Labs Enviro+ module."""
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 __app_name__ = 'f451-enviro'
